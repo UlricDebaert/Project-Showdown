d87ed718fe8ca80203d2ffea1042686f9507fa9d
Ulric <ulric.debaert@outlook.fr>
2022-08-19 12:37:32 +0200

    Bank Ready

---
+++ Project Showdown/Assets/Scripts/LineBetweenTwoPoint.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineBetweenTwoPoint : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+
+    public Transform[] linePointTrasform;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+
+    void Update()
+    {
+        for (int i = 0; i < linePointTrasform.Length; i++)
+        {
+            print("set" + linePointTrasform[i].position);
+            lineRenderer.SetPosition(i, linePointTrasform[i].position);
+        }
+    }
+}
